bc1b47b19a414b05db5b9340a2542fd6ca48ea9e
Titouan Blossier <titouan.blossier@orange.fr>
2020-10-13 11:36:44 +0100

    Cleaned Up the project a bit

--- sport/Model/Workouts.swift
+++ sport/Model/Workouts.swift
@@ -21,9 +21,8 @@ class Workouts{
         let hottie = Hottie()
         let upperBody1 = UperBodyOne()
         let upperBody2 = UpperBodyTwo()
-        let test = TestMultipleSet()
-        self.allWorkouts = [hottie, upperBody1, upperBody2, test]
-        self.allWorkoutsNames = [hottie.name, upperBody1.name, upperBody2.name, test.name]
+        self.allWorkouts = [hottie, upperBody1, upperBody2]
+        self.allWorkoutsNames = [hottie.name, upperBody1.name, upperBody2.name]
     }
     
     var allWorkoutsNames : Array<String>
